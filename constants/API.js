cc7368b295a3cf3325a4ea0fbb22068630ffcb9d
Aronwebpro <aronwebpro@gmail.com>
2018-07-15 17:08:42 -0500

    App Constants

---
+++ constants/API.js
@@ -0,0 +1,8 @@
+const CITIES_API_URI = 'https://andruxnet-world-cities-v1.p.mashape.com/';
+const CITIES_API_KEY = "aaQ6BuvVoNmshSC0gwqyotCc6ASSp1kTbXTjsnCle3yPxQCTqT";
+
+
+export {
+    CITIES_API_URI,
+    CITIES_API_KEY,
+}
